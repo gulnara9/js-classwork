a0f9961b63201b85e0d4bd2532b6a81de025af0e
gulnara <123542083+gulnara9@users.noreply.github.com>
2023-04-28 14:14:15 +0400

    Add files via upload

---
+++ 28.04-todo-app/main.js
@@ -0,0 +1,7 @@
+let addBtn=document.querySelector('#add-btn')
+let input=document.querySelector('#todo-input')
+let ul=document.querySelector('#.list')
+
+addBtn.addEventListener('click', function(){
+    
+})
